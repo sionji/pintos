b53c072310a1937a23e27bd12574b6b24c3e321d
sionji <jso61671532@gmail.com>
2020-05-29 21:28:51 +0900

    More comments

--- vm/frame.c
+++ vm/frame.c
@@ -10,173 +10,174 @@ static struct list_elem *get_next_lru_clock (void);
 struct page *
 alloc_page (enum palloc_flags flags)
 {
-	/* If palloc_get_page is failed, try to free pages. */
-	void *kaddr = palloc_get_page (flags);
-	if (kaddr == NULL)
-		kaddr = try_to_free_pages (flags);
-	
-	/* Page & Memory allocation. */
-	struct page *page = (struct page *)malloc (sizeof (struct page));
-	if (page == NULL)
-	{
-		palloc_free_page (kaddr);
-		return NULL;
-	}
+  /* If palloc_get_page is failed, try to free pages. */
+  void *kaddr = palloc_get_page (flags);
+  if (kaddr == NULL)
+    kaddr = try_to_free_pages (flags);
+  
+  /* Page & Memory allocation. */
+  struct page *page = (struct page *)malloc (sizeof (struct page));
+  if (page == NULL)
+  {
+    palloc_free_page (kaddr);
+    return NULL;
+  }
 
-	/* Initialize. */
-	page->kaddr = kaddr;
-	page->vme = NULL;
-	page->thread = thread_current ();
+  /* Initialize. */
+  page->kaddr = kaddr;
+  page->vme = NULL;
+  page->thread = thread_current ();
 
-	/* Insertion. */
-	lock_acquire (&lru_list_lock);
-	add_page_to_lru_list (page);
-	lock_release (&lru_list_lock);
+  /* Insertion. */
+  lock_acquire (&lru_list_lock);
+  add_page_to_lru_list (page);
+  lock_release (&lru_list_lock);
 
-	return page;
+  return page;
 }
 
 void
 lru_list_init (void)
 {
-	list_init (&lru_list);
-	lock_init (&lru_list_lock);
-	/* Set lru_clock value to NULL. */
-	/* !ERROR CODE! 
-		 lru_list is empty list now, so there is no element in list. 
-	   Then lru_clock may indicate NULL, 
-		 so try_to_free_pages cannot make it's entry.*/
-	//lru_clock = list_begin (&lru_list);
-	lru_clock = NULL;
+  list_init (&lru_list);
+  lock_init (&lru_list_lock);
+  /* Set lru_clock value to NULL. */
+  /* !ERROR CODE! 
+     lru_list is empty list now, so there is no element in list. 
+     Then lru_clock may indicate NULL, 
+     so try_to_free_pages cannot make it's entry.*/
+  //lru_clock = list_begin (&lru_list);
+  lru_clock = NULL;
 }
 
 void 
 add_page_to_lru_list (struct page *page)
 {
-	list_push_back (&lru_list, &page->lru);
+  list_push_back (&lru_list, &page->lru);
 }
 
 void 
 del_page_to_lru_list (struct page *page)
 {
-	list_remove (&page->lru);
+  list_remove (&page->lru);
 }
 
 static struct list_elem *
 get_next_lru_clock (void)
 {
-	lru_clock = list_next (lru_clock);
+  lru_clock = list_next (lru_clock);
 
-	if (lru_clock == list_end (&lru_list))
-		return NULL;
-	else
-		return lru_clock;
+  if (lru_clock == list_end (&lru_list))
+    return NULL;
+  else
+    return lru_clock;
 }
 
+/* No space left, try to free pages and allocate new frame. */
 void * 
 try_to_free_pages (enum palloc_flags flags)
 {
-	void *kaddr = NULL;
-	struct page *page;
-	struct vm_entry *vme;
-	if (lru_clock == NULL)
-		lru_clock = list_begin (&lru_list);
+  void *kaddr = NULL;
+  struct page *page;
+  struct vm_entry *vme;
+  if (lru_clock == NULL)
+    lru_clock = list_begin (&lru_list);
 
-	do 
-	{
-		page = list_entry (lru_clock, struct page, lru);
-		vme = page->vme;
+  do 
+  {
+    page = list_entry (lru_clock, struct page, lru);
+    vme = page->vme;
 
-		/* You must move lru_clock becasue selected page may be free. */
-		lru_clock = get_next_lru_clock ();
-		if (lru_clock == NULL)
-			return NULL;
+    /* You must move lru_clock becasue selected page may be free. */
+    lru_clock = get_next_lru_clock ();
+    if (lru_clock == NULL)
+      return NULL;
 
-	} while (vme->type == VM_FILE);
+  } while (vme->type == VM_FILE);
 
-	lock_acquire (&lru_list_lock);
-		/* Check pagedir_is_accessed. */
-		if (pagedir_is_accessed (page->thread->pagedir, page->vme->vaddr))
-			pagedir_set_accessed (page->thread->pagedir, page->vme->vaddr, false);
+  lock_acquire (&lru_list_lock);
+    /* Check pagedir_is_accessed. */
+    if (pagedir_is_accessed (page->thread->pagedir, page->vme->vaddr))
+      pagedir_set_accessed (page->thread->pagedir, page->vme->vaddr, false);
 
-		/* Victim eviction. */
-		switch (page->vme->type)
-		{
-			case VM_BIN :
-				{
-					page->vme->type = VM_ANON;
-					page->vme->swap_slot = swap_out (page->kaddr);
-					break;
-				}
+    /* Victim eviction. */
+    switch (page->vme->type)
+    {
+      case VM_BIN :
+        {
+          page->vme->type = VM_ANON;
+          page->vme->swap_slot = swap_out (page->kaddr);
+          break;
+        }
 
-			case VM_FILE :
-				{
-					/* Check pagedir_is_dirty. */
-					if (pagedir_is_dirty (page->thread->pagedir, page->vme->vaddr))
-					{
-						/* Lock must be used at every read/write operation. 
-						   Assume that if lock is acquired by other thread,
-						   then status of current thread will be changed to THREAD_BLOCK. 
-						   If you don't use lock, it will make script confused. 
-						   (lock_held_by_current_thread () script error will cause.)  */
-						lock_acquire (&filesys_lock);
-						file_write_at (vme->file, vme->vaddr, vme->read_bytes, vme->offset);
-						//pagedir_set_dirty (page->thread->pagedir, vme->vaddr, false);
-						lock_release (&filesys_lock);
-					} 
-					//page->vme->type = VM_ANON;
-					//page->vme->swap_slot = swap_out (page->kaddr);
-					break;
-				}
-	
-			case VM_ANON :
-				{
-					/* Always write at swap partition. */
-					page->vme->swap_slot = swap_out (page->kaddr);
-					break;
-				}
-		}
-		/* Free page. */
-		page->vme->is_loaded = false;
-		__free_page (page);
+      case VM_FILE :
+        {
+          /* Check pagedir_is_dirty. */
+          if (pagedir_is_dirty (page->thread->pagedir, page->vme->vaddr))
+          {
+            /* Lock must be used at every read/write operation. 
+               Assume that if lock is acquired by other thread,
+               then status of current thread will be changed to THREAD_BLOCK. 
+               If you don't use lock, it will make script confused. 
+               (lock_held_by_current_thread () script error will cause.)  */
+            lock_acquire (&filesys_lock);
+            file_write_at (vme->file, vme->vaddr, vme->read_bytes, vme->offset);
+            //pagedir_set_dirty (page->thread->pagedir, vme->vaddr, false);
+            lock_release (&filesys_lock);
+          } 
+          page->vme->type = VM_ANON;
+          page->vme->swap_slot = swap_out (page->kaddr);
+          break;
+        }
+  
+      case VM_ANON :
+        {
+          /* Always write at swap partition. */
+          page->vme->swap_slot = swap_out (page->kaddr);
+          break;
+        }
+    }
+    /* Free page. */
+    page->vme->is_loaded = false;
+    __free_page (page);
 
-		/* Memory allocation and return it's pointer.*/
-		kaddr = palloc_get_page (flags);
+    /* Memory allocation and return it's pointer.*/
+    kaddr = palloc_get_page (flags);
 
-	lock_release (&lru_list_lock);
-	return kaddr;
+  lock_release (&lru_list_lock);
+  return kaddr;
 }
 
 void
 __free_page (struct page *page)
 {
-	/* List remove. */
-	del_page_to_lru_list (page);
-	/* Free Physical page. */
-	palloc_free_page (page->kaddr);
-	/* Clear virtual page. */
-	pagedir_clear_page (page->thread->pagedir, page->vme->vaddr);
-	/* Free struct page. */
-	free (page);
+  /* List remove. */
+  del_page_to_lru_list (page);
+  /* Free Physical page. */
+  palloc_free_page (page->kaddr);
+  /* Clear virtual page. */
+  pagedir_clear_page (page->thread->pagedir, page->vme->vaddr);
+  /* Free struct page. */
+  free (page);
 
-	/* Do not free vme because vm_destroy will be called before exit PintOS. */
+  /* Do not free vme because vm_destroy will be called before exit PintOS. */
 }
 
 void 
 free_page (void *kaddr)
 {
-	struct list_elem *e, *tmp;
-	lock_acquire (&lru_list_lock);
-	for (e = list_begin (&lru_list); e != list_end (&lru_list);
-			 e = tmp)
-	{
-		struct page *page = list_entry (e, struct page, lru);
-		/* Selected list_elem will be removed from lru_list.
-		   So just change list_elem before free. */
-		tmp = list_next (e);
-		if (page->kaddr == kaddr)
-			__free_page (page);
-	}
-	lock_release (&lru_list_lock);
+  struct list_elem *e, *tmp;
+  lock_acquire (&lru_list_lock);
+  for (e = list_begin (&lru_list); e != list_end (&lru_list);
+       e = tmp)
+  {
+    struct page *page = list_entry (e, struct page, lru);
+    /* Selected list_elem will be removed from lru_list.
+       So just change list_elem before free. */
+    tmp = list_next (e);
+    if (page->kaddr == kaddr)
+      __free_page (page);
+  }
+  lock_release (&lru_list_lock);
 }
 
--- vm/swap.c
+++ vm/swap.c
@@ -8,11 +8,11 @@
 
 /* 4KB BITMAP SIZE. */
 /* If BITMAP SIZE is too large, then execution time is too long.
-	 IF BITMAP SIZE is too small, then errors will be occured. 
+   IF BITMAP SIZE is too small, then errors will be occured. 
    It is important that finding a suitable bitmap size. */
 /* To make suitable BITMAPSIZE, look at the following suggestions.
-	 1) PintOS uses 4MB swap size and PGSIZE is 4KB. 
-	 2) PintOS is operated as block. Each page has SECTORS_PER_PAGE sectors.
+   1) PintOS uses 4MB swap size and PGSIZE is 4KB. 
+   2) PintOS is operated as block. Each page has SECTORS_PER_PAGE sectors.
    So, (4MB / 4KB * SECTORS_PER_PAGE) = 2^13 BITMAPSIZE is maybe suitable. */
 #define BITMAPBITS 13
 #define BITMAPSIZE (1 << BITMAPBITS)
@@ -24,53 +24,53 @@ struct lock swap_lock;
 void 
 swap_init (void)
 {
-	block = block_get_role (BLOCK_SWAP);
-	if (block == NULL)
-		return;
+  block = block_get_role (BLOCK_SWAP);
+  if (block == NULL)
+    return;
 
-	bitmap = bitmap_create (BITMAPSIZE);
-	if (bitmap == NULL)
-		return;
+  bitmap = bitmap_create (BITMAPSIZE);
+  if (bitmap == NULL)
+    return;
 
-	bitmap_set_all (bitmap, false);
-	lock_init (&swap_lock);
+  bitmap_set_all (bitmap, false);
+  lock_init (&swap_lock);
 }
 
 void 
 swap_in (size_t used_index, void *kaddr)
 {
-	int i = 0;
+  int i = 0;
 
-	if (block == NULL || bitmap == NULL)
-		return;
+  if (block == NULL || bitmap == NULL)
+    return;
 
-	lock_acquire (&swap_lock);
-	bitmap_flip (bitmap, used_index);
+  lock_acquire (&swap_lock);
+  bitmap_flip (bitmap, used_index);
 
-	for (i = 0; i < SECTORS_PER_PAGE; i++)
-		block_read (block, used_index * SECTORS_PER_PAGE + i, 
-				        (char *) kaddr + BLOCK_SECTOR_SIZE * i);
-	lock_release (&swap_lock);
+  for (i = 0; i < SECTORS_PER_PAGE; i++)
+    block_read (block, used_index * SECTORS_PER_PAGE + i, 
+                (char *) kaddr + BLOCK_SECTOR_SIZE * i);
+  lock_release (&swap_lock);
 }
 
 size_t 
 swap_out (void *kaddr)
 {
-	int i;
-	size_t slot_num;
+  int i;
+  size_t slot_num;
 
-	if (block == NULL || bitmap == NULL)
-		return;
+  if (block == NULL || bitmap == NULL)
+    return;
 
-	lock_acquire (&swap_lock);
-	/* Find empty bitmap slot using First fit. */
-	slot_num = bitmap_scan_and_flip (bitmap, 0, 1, false);
+  lock_acquire (&swap_lock);
+  /* Find empty bitmap slot using First fit. */
+  slot_num = bitmap_scan_and_flip (bitmap, 0, 1, false);
 
-	for (i = 0; i < SECTORS_PER_PAGE; i++)
-		block_write (block, slot_num * SECTORS_PER_PAGE + i, 
-				         (char *) kaddr + BLOCK_SECTOR_SIZE * i);
-	lock_release (&swap_lock);
+  for (i = 0; i < SECTORS_PER_PAGE; i++)
+    block_write (block, slot_num * SECTORS_PER_PAGE + i, 
+                 (char *) kaddr + BLOCK_SECTOR_SIZE * i);
+  lock_release (&swap_lock);
 
-	return slot_num;
+  return slot_num;
 }
 
